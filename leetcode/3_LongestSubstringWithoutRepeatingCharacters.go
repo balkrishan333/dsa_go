5dc4da02a2cfa87d622c79e068b396e4182af78e
Balkrishan Nagpal <balkrishan.nagpal@optum.com>
2023-12-07 20:10:12 +0530

    LC-3

--- leetcode/3_LongestSubstringWithoutRepeatingCharacters.go
+++ leetcode/3_LongestSubstringWithoutRepeatingCharacters.go
@@ -1,42 +1,31 @@
 package main
 
 func lengthOfLongestSubstring(s string) int {
-
-	if len(s) == 1 {
-		return 1
+	symbols := [255]int{}
+	for i := range symbols {
+		symbols[i] = -1
 	}
 
-	indexes := make([]int, 255)
-
-	for i := 0; i < len(indexes); i++ {
-		indexes[i] = -1
-	}
-
-	length, start := 0, 0
-
-	for i := 0; i < len(s); i++ {
-		var ch byte = s[i]
-		var index int = indexes[ch]
-
-		if index != -1 && index >= start {
-			currLength := i - start
-			length = max(length, currLength)
-			start = index + 1
+	start, maxlen := 0, 0
+	for pos, char := range s {
+		//fmt.Printf("char %c pos %d\n", char, pos)
+		if symbols[char] != -1 {
+			if symbols[char] >= start {
+				if maxlen < pos-start {
+					maxlen = pos - start
+				}
+				start = symbols[char] + 1
+			}
 		}
-		indexes[ch] = i
+		symbols[char] = pos
 	}
-	length = max(length, len(s)-start)
-	return length
-}
 
-func max(val1 int, val2 int) int {
-	if val1 >= val2 {
-		return val1
+	if maxlen < len(s)-start {
+		maxlen = len(s) - start
 	}
-	return val2
+	return maxlen
 }
 
 // func main() {
-// 	s := "au"
-// 	fmt.Println(lengthOfLongestSubstring(s))
+// 	fmt.Print("Max len : ", lengthOfLongestSubstring("dvdf"), "\n")
 // }
